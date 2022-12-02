96a5b041c6e0ae0e2cd2f01232d55870df06df5c
tipakA <emka456@onet.pl>
2022-12-02 12:34:39 +0100

    Add day 1

---
+++ 2022/TS/src/days/d01.ts
@@ -0,0 +1,46 @@
+import { readFile } from 'fs/promises';
+
+function part1(lines: Array<string>) {
+  let current = 0;
+  const highest: [number, number, number] = [ 0, 0, 0 ];
+  for (const line of lines) {
+    if (line === '') {
+      current = 0;
+      continue;
+    }
+
+    const value = parseInt(line);
+    const newValue = current + value;
+    current = newValue;
+
+    if (newValue > highest[2]) {
+      highest[2] = newValue;
+    }
+    if (highest[2] > highest[1]) {
+      const temp = highest[1];
+      highest[1] = highest[2];
+      highest[2] = temp;
+    }
+    if (highest[1] > highest[0]) {
+      const temp = highest[0];
+      highest[0] = highest[1];
+      highest[1] = temp;
+    }
+  }
+
+  return { highest: highest[0], highest3: highest };
+}
+
+function part2(data: [number, number, number]) {
+  return { sum: data.reduce((a, b) => a + b, 0) };
+}
+
+export default async function d1() {
+  const file = await readFile('./input/d01.txt', 'utf8');
+  const lines = file.split('\n').slice(0, -1);
+
+  const part1Res = part1(lines);
+  const part2Res = part2(part1Res.highest3);
+
+  return { p1: part1Res.highest, p2: part2Res.sum };
+}

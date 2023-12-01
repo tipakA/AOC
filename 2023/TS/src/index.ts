d01aff38fafa956fee56715a11df0233b1a0ef0b
tipakA <emka456@onet.pl>
2023-12-01 12:07:28 +0100

    Add index.ts

---
+++ 2023/TS/src/index.ts
@@ -0,0 +1,19 @@
+import { readdir, readFile } from 'fs/promises';
+import path from 'path';
+
+async function main() {
+  const dayFiles = await readdir(path.join(__dirname, './days')).then(files => files.filter(file => file.endsWith('.js')));
+
+  for (const dayFile of dayFiles) {
+    const number = dayFile.slice(0, -3);
+    const { default: day } = await import(`./days/${dayFile}`);
+    const dayInput = await readFile(`./input/${number}.txt`, 'utf8');
+    const lines = dayInput.split('\n').slice(0, -1);
+
+    console.time(number);
+    console.log(number, await day({ input: dayInput, lines }));
+    console.timeEnd(number);
+  }
+}
+
+main();

f5c9f45eaba7628577aec863fd83114c70a3de2f
tipakA <emka456@onet.pl>
2023-12-03 02:19:44 +0100

    Day 2 part 2

--- 2023/TS/src/days/d02.ts
+++ 2023/TS/src/days/d02.ts
@@ -34,12 +34,39 @@ function part1(lines: Array<string>) {
 }
 
 function part2(lines: Array<string>) {
+  let sum = 0;
 
+  for (const game of lines) {
+    const [ , _hands ] = game.split(':');
+    const hands = _hands.split(';')
+      .map(hand => hand.trim().split(', '));
+
+    const highest = {
+      red: 0,
+      green: 0,
+      blue: 0,
+    };
+
+    for (const hand of hands) {
+      for (const aaa of hand) {
+        const [ _amount, _color ] = aaa.split(' ') as [string, keyof typeof highest];
+        const shownAmount = parseInt(_amount);
+
+        const highestSeenAmount = highest[_color];
+        if (shownAmount > highestSeenAmount) highest[_color] = shownAmount;
+      }
+    }
+
+    const power = highest.red * highest.green * highest.blue;
+    sum += power;
+  }
+
+  return sum;
 }
 
 export default function d2({ lines }: { lines: Array<string> }) {
   const part1Res = part1(lines);
   const part2Res = part2(lines);
 
-  return { p1: part1Res };
+  return { p1: part1Res, p2: part2Res };
 }

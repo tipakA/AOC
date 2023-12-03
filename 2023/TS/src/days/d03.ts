2a17c6699251247b519381bec08e6ecd8491b241
tipakA <emka456@onet.pl>
2023-12-03 23:42:50 +0100

    Day 3 part 2

--- 2023/TS/src/days/d03.ts
+++ 2023/TS/src/days/d03.ts
@@ -52,7 +52,6 @@ function part1(lines: Array<string>) {
     positions[find.idxL] = [ find.idxC - 1, find.idxC + 1 ];
     positions[find.idxL + 1] = [ find.idxC - 1, find.idxC + 1 ];
 
-
     const filtered = foundNumbers.filter(f => {
       const relevantColumns = positions[f.idxL];
       if (!relevantColumns) return false;
@@ -71,12 +70,72 @@ function part1(lines: Array<string>) {
 }
 
 function part2(lines: Array<string>) {
+  let idxL = 0;
 
+  const foundNumbers: Array<FoundNumber> = [];
+  const foundSymbols: Array<FoundSymbol> = [];
+
+  for (const line of lines) {
+    let idxC = 0;
+    const currNum = {
+      val: '',
+      pos: -1,
+    };
+
+    for (const char of line.split('')) {
+      const matchNum = char.match(/[0-9]/);
+
+      if (matchNum) {
+        currNum.val += char;
+        if (currNum.pos === -1) currNum.pos = idxC;
+      } else {
+        if (currNum.val !== '') {
+          foundNumbers.push({ idxL, idxC: currNum.pos, num: currNum.val });
+          currNum.val = '';
+          currNum.pos = -1;
+        }
+        if (char !== '.') {
+          foundSymbols.push({ idxL, idxC, symbol: char });
+        }
+      }
+
+      idxC++;
+    }
+
+    if (currNum.val !== '') foundNumbers.push({ idxL, idxC: currNum.pos, num: currNum.val });
+
+    idxL++;
+  }
+
+  let sum = 0;
+
+  for (const gear of foundSymbols.filter(f => f.symbol === '*')) {
+    const positions: Record<number, [number, number]> = {};
+    positions[gear.idxL - 1] = [ gear.idxC - 1, gear.idxC + 1 ];
+    positions[gear.idxL] = [ gear.idxC - 1, gear.idxC + 1 ];
+    positions[gear.idxL + 1] = [ gear.idxC - 1, gear.idxC + 1 ];
+
+    const filtered = foundNumbers.filter(f => {
+      const relevantColumns = positions[f.idxL];
+      if (!relevantColumns) return false;
+
+      if (relevantColumns[0] <= f.idxC + f.num.length - 1 && relevantColumns[1] >= f.idxC) return true;
+
+      // if (f.idxC >= relevantColumns[0] && (f.idxC + f.num.length - 1) <= relevantColumns[1]) return true;
+
+      return false;
+    }).map(n => parseInt(n.num));
+
+    if (filtered.length !== 2) continue;
+    sum += filtered[0] * filtered[1];
+  }
+
+  return sum;
 }
 
 export default function d3({ lines }: { lines: Array<string> }) {
   const part1Res = part1(lines);
   const part2Res = part2(lines);
 
-  return { p1: part1Res };
+  return { p1: part1Res, p2: part2Res };
 }

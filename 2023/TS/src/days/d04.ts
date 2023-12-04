2e91b7ffdbb1f26e6419917ed6b47edc3c4b2af2
tipakA <emka456@onet.pl>
2023-12-04 07:29:58 +0100

    Day 4 part 2

--- 2023/TS/src/days/d04.ts
+++ 2023/TS/src/days/d04.ts
@@ -18,19 +18,42 @@ function part1(lines: Array<string>) {
         else shouldDouble = true;
       }
     }
-
   }
 
   return sum;
 }
 
 function part2(lines: Array<string>) {
+  const cards: Record<number, number> = {};
 
+  for (const line of lines) {
+    const [ _card, _numbers ] = line.split(': ');
+    const [ , _id ] = _card.split(/ +/);
+    const [ _winning, _owned ] = _numbers.split(' | ');
+
+    const id = parseInt(_id);
+    const winning = new Set(_winning.trim().split(/ +/).map(n => parseInt(n)));
+    const owned = _owned.trim().split(/ +/).map(n => parseInt(n.trim()));
+
+    if (!cards[id]) cards[id] = 1;
+    let wins = 0;
+
+    for (const number of owned) {
+      if (winning.has(number)) {
+        wins++;
+        const currCardCount = cards[id];
+        const wonCardCountInPile = cards[id + wins] ?? 1;
+        cards[id + wins] = wonCardCountInPile + 1 * currCardCount;
+      }
+    }
+  }
+
+  return Object.values(cards).reduce((a, b) => a + b, 0);
 }
 
 export default function d4({ lines }: { lines: Array<string> }) {
   const part1Res = part1(lines);
   const part2Res = part2(lines);
 
-  return { p1: part1Res };
+  return { p1: part1Res, p2: part2Res };
 }
